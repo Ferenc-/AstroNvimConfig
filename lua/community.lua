3bff546b0e765692981c3f1a646d55485d6fb877
Ferenc Géczi <ferenc.gm@gmail.com>
2025-02-20 00:00:00 +0000

    feat: Add markdown community pack
    Signed-off-by: Ferenc Géczi <ferenc.gm@gmail.com>

--- lua/community.lua
+++ lua/community.lua
@@ -12,6 +12,7 @@ return {
   { import = "astrocommunity.pack.helm" },
   { import = "astrocommunity.pack.java" },
   { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.markdown" },
   { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.ruby" },
   { import = "astrocommunity.pack.yaml" },
